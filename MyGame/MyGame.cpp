e68e8bc11e58840cc352db630140195fa83cde2f
umutcanozer <mytheawest@gmail.com>
2025-03-02 18:25:03 +0300

    Add project files.

---
+++ MyGame/MyGame.cpp
@@ -0,0 +1,24 @@
+﻿#include "SFML/Graphics.hpp"
+
+int main()
+{
+    int w = 600; int h = 600;
+    sf::RenderWindow window(sf::VideoMode(w, h), "SFML works!");
+    sf::Event e;
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (window.pollEvent(e))
+        {
+            if (e.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
